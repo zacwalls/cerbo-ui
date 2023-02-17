2aca1ac001742af943a088a121d2fbeca49bcf3a
zwallace0790 <zwallace0790@gmail.com>
2023-02-16 21:59:47 -0800

    Had to delete cached git files for gitignore to work

--- dist/esm/types/components/forms/Button.d.ts
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export declare function Button({ children, ...props }: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>): JSX.Element;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-declare function Button({ children, ...props }: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>): JSX.Element;
-
-export { Button };
